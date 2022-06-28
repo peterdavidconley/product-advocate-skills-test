10fcc78ad775cec05dd8135e56d9352c00711793
Peter Conley <85258866+peterdavidconley@users.noreply.github.com>
2022-06-28 07:39:21 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Peter Conley Product Advocate Application<h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          1. Things a Product Advocate Might Do:
+          Here are the things a product advocate at Vercel might do in no particular order. 
+    
+          List the 3 things you would be most interested in doing:
+
+          • Create Salesforce reports to keep track of data pertinent to the team or your stats Hop on a Zoom call to do a product demo	
+          • Research and organize notes to ensure Account Executives are prepared for calls
+          • Write direct and concise emails that dig into a prospect's pain points and needs 
+    
+          3 you would be least interested in doing:
+    
+          • Evaluate OSS sponsorship applications
+          • Evaluate an application to help determine if it's commercial or not
+          • Attend weekly enablement meetings to level up your product and sales knowledge
+    
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
