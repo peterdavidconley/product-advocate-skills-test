5a95f679c0b5e96560e010d1c30fa2dd2aaf7005
Peter Conley <85258866+peterdavidconley@users.noreply.github.com>
2022-06-28 07:43:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,8 +21,55 @@ function App() {
           • Evaluate OSS sponsorship applications
           • Evaluate an application to help determine if it's commercial or not
           • Attend weekly enablement meetings to level up your product and sales knowledge
+         </p>
+         <p>
+          2. Reflection on past experience:	
     
-        </p>
+          a. In your next role, what are you looking to learn or do more of?
+							
+          b. Which (non-Vercel) technical or sales article, guide or lightning talk left the biggest positive impression on you? Why? (and please provide             the link)
+			
+         </p>
+         <p>
+         3. Using Next.js
+  
+         Add a temporary redirect to your Next/Vercel project
+							
+         Add a temporary in-app redirect to your project by redirecting /hello-vercel (or any path – but be sure to tell us which!) to https://vercel.com
+
+         </p>
+         <p>
+    
+          4. Practice Email Responses
+          Please include one email response for each inquiry, just as if you were responding to a customer.
+							
+          Sales Inquiry: Do I need Enterprise?
+            
+          Subject: Sales Lead for barnesandnoble.com
+          Name: Joe Fox
+          Employees: 1000+
+            
+          How can we help you: New site launching soon using Next.js and we are thinking about using Vercel to host. I would like more information about             the Enterprise option.
+
+          Sales Inquiry: Would Pro or Enterprise fit better for my team? 
+          Subject: Sales Lead for hipcamp.com
+          Name: Melissa Wright
+          Employees: 10-100
+
+          How can we help you: We are looking to re-platform our site and are considering Vercel. Would like to evaluate options to see if Pro or                     Enterprise would fit out needs better. Timeline is Q3. Can you help?	
+            
+         </p>
+         <p>
+         							
+          5. Your personal recommendations:
+          When it comes to looking for a solution for hosting your Next.js site, what are some of the common things
+
+          you would value as the Engineering Manager for:
+          1. A mom and pop business?
+          2. An NFT?
+          3. A Marketing Page for a large corporation like Nintendo?
+
+         </p>
       </header>
     </div>
   );
